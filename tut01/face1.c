bbf062c50503270aa355dfad3d31d2d3ad00ae6d
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-09-17 13:46:21 +1000

    Added tutorial code

---
+++ tut01/face1.c
@@ -0,0 +1,23 @@
+// 
+// 17th September, 2021
+// By F11C
+// 
+// Prints out a face in the shape:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+
+    return 0;
+}
+
